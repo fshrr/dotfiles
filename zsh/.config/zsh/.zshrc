2ececeb3a144f491597eb9f9fbadd3c1eb22b4b1
fshrr <fahim.shahreer95@gmail.com>
2026-09-20 05:13:54 -0400

    Unalias zi from zinit to use zoxide's zi

--- zsh/.config/zsh/.zshrc
+++ zsh/.config/zsh/.zshrc
@@ -3,3 +3,5 @@ for conf in "$HOME"/.config/zsh/[0-9]*.zsh; do
   source "$conf"
 done
 unset conf
+
+if command -v wt >/dev/null 2>&1; then eval "$(command wt config shell init zsh)"; fi
--- zsh/.config/zsh/02-plugins.zsh
+++ zsh/.config/zsh/02-plugins.zsh
@@ -4,6 +4,9 @@ ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 source "${ZINIT_HOME}/zinit.zsh"
 
+# Unalias zi to be able to use zi for zoxide's interactive mode
+zinit ice atload'unalias zi'
+
 # Plugins
 zinit light zsh-users/zsh-syntax-highlighting
 zinit light zsh-users/zsh-completions
